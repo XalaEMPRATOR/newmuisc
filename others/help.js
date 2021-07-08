30af5e03a89e899a399f1690f84b1a18ad3967a7
poyzun <78980892+XalaEMPRATOR@users.noreply.github.com>
2021-07-08 19:10:42 +0000

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,138 +1,45 @@
+
 const { Client, Collection, MessageEmbed } = require(`discord.js`);
 const { 
   PREFIX, 
-  approveemoji,
-  denyemoji 
 } = require(`../config.json`);
 
+  
+
+
 module.exports = {
-  name: `help`,
-  description: `Gives you a list of all help Commands`,
-  aliases: ["h","commands"],
-  cooldown: 3,
-  edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
-  execute(message,args,client) {
-     
+  name: "help",
+  aliases: ["h"],
+  cooldown: 10,
+  description: "**all commands**",
+  execute(message) {
     let commands = message.client.commands.array();
- 
+
     let helpEmbed = new MessageEmbed()
-      .setTitle("Harmony Help")
-      .setAuthor('Click here to invite me!!', 'https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif', 'https://discord.com/api/oauth2/authorize?client_id=767885987740254291&permissions=49572160&scope=bot')
-      .setDescription(`**PREFIX:** \`${PREFIX}\``)
-      .setFooter( client.user.username +`Type: ${PREFIX}help <Command>  for more information!`, "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif")
-      .setColor("#F0EAD6");
-
-      let ifargstruedothis = -1;
-      
-      switch(args[0]){
-          case "filter":
-           ifargstruedothis=0;
-          break;
-          case "loop":
-            ifargstruedothis=1;
-          break;
-          case "lyrics":
-            ifargstruedothis=2
-          break;
-          case "nowplaying":
-            ifargstruedothis=3
-          break;
-          case "pause":
-            ifargstruedothis=4
-          break;
-          case "play":
-            ifargstruedothis=5
-          break;
-          case "playlist":
-            ifargstruedothis=6
-          break;
-          case "queue":
-            ifargstruedothis=7
-          break;
-          case "radio":
-            ifargstruedothis=8
-          break;
-          case "remove":
-            ifargstruedothis=9
-          break;
-          case "resume":
-            ifargstruedothis=10
-          break;
-          case "search":
-            ifargstruedothis=11
-          break;
-          case "shuffle":
-            ifargstruedothis=12
-          break;
-          case "skip":
-            ifargstruedothis=13
-          break;
-          case "skipto":
-            ifargstruedothis=14
-          break;
-          case "stop":
-            ifargstruedothis=15
-          break;
-          case "volume":
-            ifargstruedothis=16
-          break;
-          case "help":
-            ifargstruedothis=17
-          break;    
-          case "invite":
-            ifargstruedothis=18
-          break;
-          case "ping":
-            ifargstruedothis=19
-          break;
-          case "prefix":
-            ifargstruedothis=20
-          break;
-          case "uptime":
-            ifargstruedothis=21
-          break;
-          case "botlist":
-            ifargstruedothis=22
-          break;
-          default:        
-            commands.forEach((cmd) => {
-              helpEmbed.addField(
-                `**${message.client.prefix}${cmd.name}**`,
-                `${cmd.description}`,
-                true
-              );
-            });
-          if(!message.guild) {
-            if(!args[0]) {message.react(approveemoji);return message.channel.send(helpEmbed);}
-            return
-            }
-            message.channel.send(helpEmbed);
-           
-        break;
-       }
-     
-       if(ifargstruedothis>=0){
-         let aliases = commands[ifargstruedothis].aliases;
-         if(aliases === undefined || !aliases) aliases="No Aliases!";
-         let cooldown = commands[ifargstruedothis].cooldown;
-         if(cooldown === undefined || !cooldown) cooldown="No Cooldown!";
+    .setThumbnail(`https://media.discordapp.net/attachments/798087155347226625/817669570587787294/2ad6ce1bd5a55dc9848cf81838c8d6a6.gif `)
+    .setTitle(`PREFIX [/]
+List of all commands`)
+    .setDescription(`**
+<a:emoji_7:843515052438323240>User Commands<a:emoji_7:843515052438323240>
+> \`invite\` - \`about\`
+> \`ping\` - \`prefix\` - \`uptime\`
+<a:emoji_2:842511069180198972>Music Commands<a:emoji_2:842511069180198972>
+> \`play\` - \`skip\` - \`skipto\`
+> \`stop\` - \`volume\` - \`nowplaying\`
+> \`shuffle\` - \`search\` - \`resume\`
+> \`remove\` - \`queue\` - \`filter\`
+> \`loop\` - \`lyrics\` - \`radio\`
+<a:emoji_5:842511173101813760>Fun Command<a:emoji_5:842511173101813760>
+> lock - unlock - kick
+> server - emoji - id
+> avatar - unban - ban
+<a:emoji_4:842511148645482559>About Bot<a:emoji_4:842511148645482559>
+[Support](https://discord.gg/rD2eeyQAaT) - [Invite](https://discord.com/api/oauth2/authorize?client_id=842355185912512582&permissions=8&scope=bot)**`)
 
 
-        helpEmbed.addField(
-          `**${message.client.prefix}${commands[ifargstruedothis].name}**`,
-          `\`\`\`fix\n${commands[ifargstruedothis].edesc}\n\`\`\`\n\`${commands[ifargstruedothis].description}\``
-        );
-        helpEmbed.addField(
-          `**:notes: Aliases:**`,
-          `\`${aliases}\``
-        );
-        helpEmbed.addField(
-          `**:wrench: Cooldown:**`,
-          `\`${cooldown}\``
-        );
-        if(!message.guild) return message.channel.send(helpEmbed);
-          message.channel.send(helpEmbed);
-       }
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM")
+    return message.channel.send(helpEmbed).catch(console.error);
 
-}
-}
+  }
+};
